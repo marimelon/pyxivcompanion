7a302ad0b3170f0486190b545cab4cf935950283
marimelon <29140293+marimelon@users.noreply.github.com>
2021-02-23 00:53:25 +0900

    update app version

--- pyxivcompanion/config.py
+++ pyxivcompanion/config.py
@@ -1,7 +1,7 @@
 from .utility import classproperty
 
 class Config:
-    APP_VERSION = "1.11.0"
+    APP_VERSION = "1.11.1"
     GLOBAL_COMPANION_BASE_URL = 'https://companion.finalfantasyxiv.com/'
     SIGHT_PATH = 'sight-v060/sight/'
 
