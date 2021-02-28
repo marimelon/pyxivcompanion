182cb51ea017dc9ecb7b535c7681c16b0986dea2
marimelon <29140293+marimelon@users.noreply.github.com>
2021-03-01 02:45:00 +0900

    add addressbook get_character_details

--- pyxivcompanion/address_book.py
+++ pyxivcompanion/address_book.py
@@ -12,6 +12,46 @@ class OnlineStatusResponse(BaseModel):
     characters: list[str]
 
 
+class CharacterDetailsCharacterSkill(BaseModel):
+    code: str
+    level: int
+
+
+class CharacterDetailsGC(BaseModel):
+    gcid: int
+    gcRank: int
+
+
+class CharacterDetailsFCCrest(BaseModel):
+    base: str
+    frame: str
+    symbol: str
+
+
+class CharacterDetailsFC(BaseModel):
+    name: str
+    abbr: str
+    crest: CharacterDetailsFCCrest
+
+
+class CharacterDetailsCharacter(BaseModel):
+    cid: str
+    name: str
+    world: str
+    portrait: str
+    skills: list[CharacterDetailsCharacterSkill]
+    gc: CharacterDetailsGC
+    fc: CharacterDetailsFC
+    classJob: str
+    jobStone: list[str]
+    lodestonecid: str
+
+
+class CharacterDetails(BaseModel):
+    updatedAt: int
+    character: CharacterDetailsCharacter
+
+
 class AddressBook:
     @staticmethod
     async def get_online_states(token: Token):
@@ -23,5 +63,18 @@ class AddressBook:
         if res.status == 200:
             data = await res.json()
             return await OnlineStatusResponse(**data), res
+        else:
+            raise await CompanionErrorResponse.select(res)
+
+    @staticmethod
+    async def get_character_details(cid: str, token: Token, updatedAt: int = 0):
+        """GET /address-book/{cid}/profile?updatedAt={updatedAt}"""
+        req = CompanionRequest(url=f'{token.region}{Config.SIGHT_PATH}address-book/{cid}/profile',
+                               RequestID=str(uuid.uuid4()).upper(),
+                               Token=token.token)
+        res = await req.get(params={'updatedAt': updatedAt})
+        if res.status == 200:
+            data = await res.json()
+            return await CharacterDetails(**data), res
         else:
             raise await CompanionErrorResponse.select(res)
